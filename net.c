475633d45a6eb257b67802f200e41bba0fcf6530
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:47:47 +0900

    Skeleton of step3

--- net.c
+++ net.c
@@ -7,11 +7,19 @@
 #include "util.h"
 #include "net.h"
 
+struct net_protocol
+{
+    struct net_protocol *next;
+    uint16_t type;
+    net_protocol_handler_t handler;
+};
+
 /*
  * NOTE: if you want to add/delete the entries after net_run(),
  *       you need to protect these lists with a lock.
  */
 static struct net_device *devices;
+static struct net_protocol *protocols;
 
 struct net_device *
 net_device_alloc(void)
@@ -82,6 +90,20 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
         errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
         return -1;
     }
+    return 0;
+}
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int net_protocol_register(uint16_t type, net_protocol_handler_t handler)
+{
+}
+
+int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
+{
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
     return 0;
 }
 
