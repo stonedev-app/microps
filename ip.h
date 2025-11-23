24beaba03e3b46a4e711acc7e352cfdd5ee323f0
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-23 09:14:30 +0000

    Revert "Skeleton of step3"
    This reverts commit 475633d45a6eb257b67802f200e41bba0fcf6530.

--- ip.h
+++
@@ -1,7 +0,0 @@
-#ifndef IP_H
-#define IP_H
-
-extern int
-ip_init(void);
-
-#endif
--- net.h
+++ net.h
@@ -8,30 +8,22 @@
 #define IFNAMSIZ 16
 #endif
 
-#define NET_DEVICE_TYPE_DUMMY 0x0000
-#define NET_DEVICE_TYPE_LOOPBACK 0x0001
-#define NET_DEVICE_TYPE_ETHERNET 0x0002
+#define NET_DEVICE_TYPE_DUMMY     0x0000
+#define NET_DEVICE_TYPE_LOOPBACK  0x0001
+#define NET_DEVICE_TYPE_ETHERNET  0x0002
 
-#define NET_DEVICE_FLAG_UP 0x0001
-#define NET_DEVICE_FLAG_LOOPBACK 0x0010
+#define NET_DEVICE_FLAG_UP        0x0001
+#define NET_DEVICE_FLAG_LOOPBACK  0x0010
 #define NET_DEVICE_FLAG_BROADCAST 0x0020
-#define NET_DEVICE_FLAG_P2P 0x0040
-#define NET_DEVICE_FLAG_NEED_ARP 0x0100
+#define NET_DEVICE_FLAG_P2P       0x0040
+#define NET_DEVICE_FLAG_NEED_ARP  0x0100
 
 #define NET_DEVICE_ADDR_LEN 16
 
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "UP" : "DOWN")
 
-/*
- * NOTE: use same value as the Ethernet types
- */
-#define NET_PROTOCOL_TYPE_IP 0x0800
-#define NET_PROTOCOL_TYPE_ARP 0x0806
-#define NTT_PROTOCOL_TYPE_IPV6 0x86dd
-
-struct net_device
-{
+struct net_device {
     struct net_device *next;
     unsigned int index;
     char name[IFNAMSIZ];
@@ -44,20 +36,12 @@ struct net_device
     uint8_t broadcast[NET_DEVICE_ADDR_LEN];
 };
 
-typedef void (*net_protocol_handler_t)(const uint8_t *data, size_t len, struct net_device *dev);
-
 extern struct net_device *
 net_device_alloc(void);
 extern int
 net_device_register(struct net_device *dev);
 extern int
 net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
-
-extern int
-net_protocol_register(uint16_t type, net_protocol_handler_t handler);
-
-extern int
-net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev);
 
 extern int
 net_init(void);
