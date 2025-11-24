129e1457c85a5336dc72d6762ebbb89829774ff5
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-24 03:19:31 +0000

    Completion of step2

--- net.c
+++ net.c
@@ -51,6 +51,14 @@ net_device_open(struct net_device *dev)
         errorf("already opened, dev=%s", dev->name);
         return -1;
     }
+    if (dev->ops->open)
+    {
+        if (dev->ops->open(dev) == -1)
+        {
+            errorf("failure, dev=%s", dev->name);
+            return -1;
+        }
+    }
     dev->flags |= NET_DEVICE_FLAG_UP;
     return 0;
 }
@@ -63,6 +71,14 @@ net_device_close(struct net_device *dev)
     {
         errorf("not opened, dev=%s", dev->name);
         return -1;
+    }
+    if (dev->ops->close)
+    {
+        if (dev->ops->close(dev) == -1)
+        {
+            errorf("failure, dev=%s", dev->name);
+            return -1;
+        }
     }
     dev->flags &= ~NET_DEVICE_FLAG_UP;
     return 0;
@@ -82,11 +98,24 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
         errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
         return -1;
     }
+    if (!dev->ops->output)
+    {
+        errorf("output callback function is not set, dev=%s", dev->name);
+        return -1;
+    }
+    if (dev->ops->output(dev, type, data, len, dst) == -1)
+    {
+        errorf("failure, dev=%s, len=%zu", dev->name, len);
+        return -1;
+    }
     return 0;
 }
 
 int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    return 0;
 }
 
 int net_init(void)
--- net.h
+++ net.h
@@ -8,22 +8,23 @@
 #define IFNAMSIZ 16
 #endif
 
-#define NET_DEVICE_TYPE_DUMMY     0x0000
-#define NET_DEVICE_TYPE_LOOPBACK  0x0001
-#define NET_DEVICE_TYPE_ETHERNET  0x0002
+#define NET_DEVICE_TYPE_DUMMY 0x0000
+#define NET_DEVICE_TYPE_LOOPBACK 0x0001
+#define NET_DEVICE_TYPE_ETHERNET 0x0002
 
-#define NET_DEVICE_FLAG_UP        0x0001
-#define NET_DEVICE_FLAG_LOOPBACK  0x0010
+#define NET_DEVICE_FLAG_UP 0x0001
+#define NET_DEVICE_FLAG_LOOPBACK 0x0010
 #define NET_DEVICE_FLAG_BROADCAST 0x0020
-#define NET_DEVICE_FLAG_P2P       0x0040
-#define NET_DEVICE_FLAG_NEED_ARP  0x0100
+#define NET_DEVICE_FLAG_P2P 0x0040
+#define NET_DEVICE_FLAG_NEED_ARP 0x0100
 
 #define NET_DEVICE_ADDR_LEN 16
 
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "UP" : "DOWN")
 
-struct net_device {
+struct net_device
+{
     struct net_device *next;
     unsigned int index;
     char name[IFNAMSIZ];
@@ -34,9 +35,12 @@ struct net_device {
     uint16_t alen;
     uint8_t addr[NET_DEVICE_ADDR_LEN];
     uint8_t broadcast[NET_DEVICE_ADDR_LEN];
+    struct net_device_ops *ops;
+    void *priv;
 };
 
-struct net_device_ops {
+struct net_device_ops
+{
     int (*open)(struct net_device *dev);
     int (*close)(struct net_device *dev);
     int (*output)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
